31eaff5f14ed5f984299e5e92df90390eb7eda01
Sanjay S <sanjayajay709@gmail.com>
2026-09-12 12:18:32 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -53,7 +53,7 @@ export default function Navbar() {
               "
             >
               <img
-                src="WhatsApp Image 2026-09-12 at 9.19.32 AM.jpeg"
+                src="frontend/public/WhatsApp Image 2026-09-12 at 9.19.32 AM.jpeg"
                 alt="Golden Cricket Club"
                 className="
                   w-full
